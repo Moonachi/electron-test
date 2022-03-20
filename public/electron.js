cfd15bc412f6c61ec70cc7a31b223e7b9609c178
최문혁(choimoonhyuk) <V12796@Vatechnetworks.com>
2022-03-20 13:29:33 +0900

    asd

--- public/electron.js
+++ public/electron.js
@@ -1,21 +1,26 @@
-const { app, BrowserWindow, ipcMain } = require("electron");
+const { app, BrowserWindow, ipcMain, dialog  } = require("electron");
 const path = require("path");
 const isDev = require("electron-is-dev");
 const { autoUpdater } = require("electron-updater");
 const log = require("electron-log");
-const ProgressBar = require("electron-progressbar")
+const ProgressBar = require('electron-progressbar');
+const axios = require("axios");
+const { URLSearchParams } = require("url");
 
 let mainWindow;
 let progressBar;
 
 function createWindow() {
+  console.log("# __dirname", path.join(__dirname, "preload.js"))
   mainWindow = new BrowserWindow({
     width: 900,
     height: 680,
     webPreferences: {
       nodeIntegration: true,
+      contextIsolation:false,
       enableRemoteModule: true,
-      devTools: isDev
+      devTools: isDev,
+      preload: path.join(__dirname, "preload.js")
     }
   });
 
@@ -34,12 +39,33 @@ function createWindow() {
   mainWindow.focus();
 }
 
+ipcMain.on("app_version", event => {
+  event.reply("app_version", { version: app.getVersion() });
+})
+
+ipcMain.on("get_data", (event, args) => {
+  const { protocol, id, secret, ip, port } = args;
+  const url = protocol + "://" + ip + ":" + port + "/oauth/token";
+  const qs = "client_id=" + id + "&client_secret=" + secret + "&grant_type=client_credentials";
+  log.info("url : ", url)
+
+  axios.post(url, {
+data
+  },
+  {  headers: {
+    "Content-Type":"application/x-www-form-urlencoded"
+  }})
+  // event.reply("app_version", { version: app.getVersion() });
+})
+
+      
+autoUpdater.autoDownload = false;
 
 autoUpdater.on("checking-for-update", () => {
-  log.info("업데이트 확인 중...");
-});
-autoUpdater.on("update-available", info => {
-  log.info("업데이트가 가능합니다.", info);
+  log.info("업데이트 체크");
+})
+
+autoUpdater.on('update-available', () => {
   dialog
     .showMessageBox({
       type: 'info',
@@ -54,6 +80,11 @@ autoUpdater.on("update-available", info => {
       if (buttonIndex === 0) autoUpdater.downloadUpdate();
     });
 });
+
+autoUpdater.on("update-not-available", () => {
+  log.info("update-not-available");
+})
+
 autoUpdater.once('download-progress', (progressObj) => {
   progressBar = new ProgressBar({
     text: 'Downloading...',
@@ -70,12 +101,6 @@ autoUpdater.once('download-progress', (progressObj) => {
     });
 });
 
-autoUpdater.on("update-not-available", info => {
-  log.info("현재 최신버전입니다.", info);
-});
-autoUpdater.on("error", err => {
-  log.info("에러가 발생하였습니다. 에러내용 : " + err);
-});
 autoUpdater.on('update-downloaded', () => {
   progressBar.setCompleted();
   dialog
--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { useEffect, useState } from "react"
+
+const { ipcRenderer } = window
+
 const App = () => {
-  return <div>Electron 업데이트</div>
+  const [version, setVersion] = useState(0);
+  const [protocol, setProtocol] = useState("");
+  const [ip, setIp] = useState("");
+  const [port, setPort] = useState("")
+
+  useEffect(() => {
+    ipcRenderer.send("app_version");
+
+    ipcRenderer.on("app_version", (event, args) => {
+      setVersion(args.version);
+    })
+  }, [])
+
+  return <div>
+    <span>Electron Version {version}</span>
+    <input value={protocol} onChange={e => setProtocol(e.target.value)} />
+    <input value={ip} onChange={e => setIp(e.target.value)} />
+    <input value={port} onChange={e => setPort(e.target.value)} />
+    <button onClick={() => {
+      ipcRenderer.send("get_data", { ip, port })
+    }}>데이터 가져오기</button>
+  </div>
 }
 
 export default App
